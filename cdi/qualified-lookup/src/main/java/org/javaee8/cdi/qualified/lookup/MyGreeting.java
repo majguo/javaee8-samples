47f5cc74f7aebc9229361a7c8e3e168999848113
Jianguo Ma <jiangma@microsoft.com>
2025-06-26 07:14:16 +0800

    refactor to single module project and use payara-managed for arquillian test
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- cdi/qualified-lookup/src/main/java/org/javaee8/cdi/qualified/lookup/MyGreeting.java
+++
@@ -1,5 +0,0 @@
-package org.javaee8.cdi.qualified.lookup;
-
-public interface MyGreeting {
-    String getGreet();
-}
--- ejb/remote/roles-allowed/src/main/java/org/javaee8/ejb/remote/remote/BeanRemote.java
+++
@@ -1,9 +0,0 @@
-/** Copyright Payara Services Limited **/
-package org.javaee8.ejb.remote.remote;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface BeanRemote {
-    String method();
-}
